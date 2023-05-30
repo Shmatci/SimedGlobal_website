d7569142f1c9a44a5f5311b1e747a1c49c98484e
Shmatci <tamara.vuksan8@gmail.com>
2023-05-30 12:10:03 +0100

    before build

--- Global_nextjs-master/components/home-page/home-1/Block.jsx
+++ Global_nextjs-master/components/home-page/home-1/Block.jsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 const Block = () => {
   return (
     <>
       <div className="row align-items-end" data-aos="fade-up">
         <div className="col-md-5 col-sm-4">
-          <img
+          <Image
             src="/images/shape/shape_136.svg"
             alt="shape"
             className="lazy-img d-none d-sm-inline-block"
+            width={100}
+            height={100}
           />
         </div>
         <div className="col-md-7 col-sm-8 wow fadeInDown">
@@ -43,10 +47,12 @@ const Block = () => {
             <p>Say something.</p>
           </div>
           {/* /.block-wrapper */}
-          <img
+          <Image
             src="/images/shape/shape_137.svg"
             alt="shape"
             className="lazy-img mt-30 ms-auto me-auto d-none d-sm-inline-block"
+            width={100}
+            height={100}
           />
         </div>
       </div>
