2bc1f260445df469583e22fe2f18ddad13269387
Shmatci <tamara.vuksan8@gmail.com>
2023-06-19 12:40:43 +0100

    again

--- Global_nextjs-master/components/footer/NewsLetter.jsx
+++ Global_nextjs-master/components/footer/NewsLetter.jsx
@@ -1,28 +1,14 @@
-import { useRef } from 'react'
-
 const NewsLetter = () => {
-  const inputRef = useRef(null)
-
-  const subscribeUser = async (event) => {
-    event.preventDefault();
-
-    const res = await fetch('/api/subscribeUser', {
-      body: JSON.stringify({
-        email: inputRef.current.value,
-      }),
-
-      headers: {
-        'Content-Type': 'application/json',
-      },
-
-      method: 'POST',
-    }); 
+  const handleSubmit = (event) => {
+    event.preventDefault(); // prevent default form submission behavior
+    // handle form submission logic
+    
   };
 
   return (
-    <form onClick={subscribeUser} className="position-relative">
-      <input type="email" ref={inputRef} placeholder="Enter your email" required />
-      <button className="tran3s fw-500 position-absolute" type='submit'>Subscribe</button>
+    <form onClick={handleSubmit} className="position-relative">
+      <input type="email" placeholder="Enter your email" required />
+      <button className="tran3s fw-500 position-absolute">Sign Up</button>
     </form>
   );
 };
