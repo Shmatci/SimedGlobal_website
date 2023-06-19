dded1203b169176d3ba3c4ccbf185c56a68386d0
Shmatci <tamara.vuksan8@gmail.com>
2023-06-19 12:21:37 +0100

    adding newsletter pop-up

--- Global_nextjs-master/pages/_document.js
+++ Global_nextjs-master/pages/_document.js
@@ -14,6 +14,13 @@ export default function Document() {
         />
 
         <link rel="icon" href="/images/logo/site_icon-1.jpg" />
+        <script id="mcjs">
+          
+          !function(c,h,i,m,p)
+          {`m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)`}
+          (document,&apos;script&apos;,&apos;https://chimpstatic.com/mcjs-connected/js/users/156928e47428534a663feffa5/6ae7e1d72bb2a972367789545.js&apos;);
+        
+          </script>
       </Head>
       <body>
         <Main />
